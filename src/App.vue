2182cab8972b28ad48d1514c6af4031e6970b77e
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-15 15:50:20 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HAPI Art</h1>
   </div>
 </template>
 
@@ -12,7 +11,13 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      paintings: [],
+      error: ''
+    }
+  },
 }
 </script>
 
