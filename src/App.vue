b185c51a225b6226246d51166ca8d88e859e0b60
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-15 19:41:55 -0600

    Update styling and refactor buttons to be in Art

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <h1>HAPI Art</h1>
-    <p class="App--p">It’s not everyday you can visit the Havard Art Museum. Here’s a peak inside.</p>
-    <div class="App--div">
-    <button 
-      :disabled="isPrevDisabled" 
-      v-on:click="moveToPrevImg"
-    ><i class="fas fa-arrow-circle-left"       :disabled="isPrevDisabled" 
-      v-on:click="moveToPrevImg"></i></button>
-    
-    <Art :key="paintings[currentImage].id" v-if="paintings.length" :art="paintings[currentImage]"/>
-    <button 
-    :disabled="isNextDisabled"
-    v-on:click="moveToNextImg"
-    ><i class="fas fa-arrow-circle-right"></i></button>
-    </div>
+    <header>
+      <h1>HAPI Art</h1>
+      <p class="App--p">It’s not everyday you can visit the Havard Art Museum. Here’s a peak inside.</p>
+    </header>
+    <Art 
+      :key="paintings[currentImage].id" 
+      v-if="paintings.length" 
+      :art="paintings[currentImage]"
+      v-on:move-prev="moveToPrevImg"
+      v-on:move-next="moveToNextImg"
+      :prevDisabled="isPrevDisabled"
+      :nextDisabled="isNextDisabled"
+    />
   </div>
 </template>
 
@@ -73,47 +71,37 @@ export default {
   *,
   *::before,
   *::after {
-  box-sizing: border-box;
+    box-sizing: border-box;
   }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, 'Font Awesome\ 5 Free', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, 'Font Awesome\ 5 Free', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    box-sizing: border-box;
+    margin: 0;
+  }
 
-  box-sizing: border-box;
-}
+  body {
+    margin: 0;
+  }
 
-h1 {
-  font-size: 40px;
-}
+  h1 {
+    font-size: 40px;
+    margin: 0;
+  }
 
-.App--p {
-  font-size: 22px;
-  font-weight: medium;
-}
+  header {
+    box-shadow: -11px -9px 2px 0px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0, 0, 0, 0.06);
+    height: 140px;
+    padding-top: 10px;
+    background-color: #f1ebe7;
+    margin-bottom: 20px;
+  }
 
-.App--div {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-button {
-  border: none;
-  font-size: 25px;
-  color: black;
-  cursor: pointer;
-}
-
-button:hover, button:disabled {
-  color: darkgray;
-}
-
-button:disabled {
-  cursor: not-allowed;
-}
-
+  .App--p {
+    font-size: 20px;
+    font-weight: medium;
+  }
 </style>
