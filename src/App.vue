8f251fe3c48eadddc5122c33ff3b2971bb0986cf
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-15 18:39:14 -0600

    Add left and right arrow icons to move through pics

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <h1>HAPI Art</h1>
-    <h3>It’s not everyday you can visit the Havard Art Museum, so here’s a peak inside</h3>
+    <p class="App--p">It’s not everyday you can visit the Havard Art Museum. Here’s a peak inside.</p>
+    <div class="App--div">
     <button 
       :disabled="isPrevDisabled" 
       v-on:click="moveToPrevImg"
-    >Previous</button>
+    ><i class="fas fa-arrow-circle-left"       :disabled="isPrevDisabled" 
+      v-on:click="moveToPrevImg"></i></button>
+    
     <Art :key="paintings[currentImage].id" v-if="paintings.length" :art="paintings[currentImage]"/>
     <button 
     :disabled="isNextDisabled"
     v-on:click="moveToNextImg"
-    >Next</button>
+    ><i class="fas fa-arrow-circle-right"></i></button>
+    </div>
   </div>
 </template>
 
@@ -66,13 +70,50 @@ export default {
 </script>
 
 <style>
+  *,
+  *::before,
+  *::after {
+  box-sizing: border-box;
+  }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, 'Font Awesome\ 5 Free', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
   box-sizing: border-box;
 }
+
+h1 {
+  font-size: 40px;
+}
+
+.App--p {
+  font-size: 22px;
+  font-weight: medium;
+}
+
+.App--div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  border: none;
+  font-size: 25px;
+  color: black;
+  cursor: pointer;
+}
+
+button:hover, button:disabled {
+  color: darkgray;
+}
+
+button:disabled {
+  cursor: not-allowed;
+}
+
 </style>
