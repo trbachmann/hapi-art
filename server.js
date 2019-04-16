198797a5a6415c9e0ac4a8128e8ba556fcc7eddf
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-16 16:57:34 -0600

    Update server file for deploy

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, (res, req) => {
   res.sendfile(__dirname + "/dist/index.html")
 });
-app.listen(port)
+app.listen(port);
