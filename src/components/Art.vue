e1835e636fa4eb99e77fd36ab7797dc816f4013d
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-04-15 18:38:50 -0600

    Update styles for Art and app credit line and people information

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="Art">
-    <p>{{ art.title }}</p>
+    <h3>{{ art.title }}</h3>
     <img v-if="art.imagecount" :src="art.images[0].baseimageurl" :alt="art.title" />
+    <div v-for="person in art.people">
+      <p>People:</p>
+      <p :person="person">{{ person.displayname }}</p>
+    </div>
+    <p>Credit: {{ art.creditline }}</p>
   </div>
 </template>
 
@@ -15,8 +20,18 @@ export default {
 </script>
 
 <style scoped>
- img {
-   height: 150px;
-   width: 200px;
+  .Art {
+    display: inline-block;
+    width: 500px;
+  }
+
+  h3 {
+    width: 450px;
+    height: 100px;
+  }
+ img {  
+    object-fit: contain;
+    height: 375px;
+    width: 450px;
  }
 </style>
